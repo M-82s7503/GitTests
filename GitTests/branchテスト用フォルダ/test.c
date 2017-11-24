6bbe6ee7eba241fad1f850214a8509ac8e262a0c
N-61336535R-N <N.math.mryo.N@gmail.com>
2017-11-25 00:29:17 +0900

    branch テスト（branchにコミットしてみる）
    ターミナル上で一旦、
      git checkout [branch名]
    を実行して、そのbranchに入らないと、
    Xcode 側は認識してくれないっぽい。

---
+++ GitTests/branchテスト用フォルダ/test.c
@@ -0,0 +1,9 @@
+//
+//  test.c
+//  GitTests
+//
+//  Created by Matsumoto Ryo on 2017/11/25.
+//  Copyright © 2017年 Univ-ryukyu. All rights reserved.
+//
+
+#include "test.h"
---
+++ GitTests/branchテスト用フォルダ/test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  GitTests
+//
+//  Created by Matsumoto Ryo on 2017/11/25.
+//  Copyright © 2017年 Univ-ryukyu. All rights reserved.
+//
+
+#ifndef test_h
+#define test_h
+
+#include <stdio.h>
+
+#endif /* test_h */
