87b0e9d704d98d06103bb342373b1a2d69d66a02
N-61336535R-N <N.math.mryo.N@gmail.com>
2017-12-11 21:41:59 +0900

    pullreq テスト用のcommit
    とりあえず、develop ブランチを更新。
    masterにpull req を送るための準備

---
+++ pullreqTest.h
@@ -0,0 +1,15 @@
+//
+// Created by Matsumoto Ryo on 2017/12/11.
+// Copyright (c) 2017 Univ-ryukyu. All rights reserved.
+//
+
+#ifndef GITTESTS_PULLREQTEST_H
+#define GITTESTS_PULLREQTEST_H
+
+
+class pullreqTest {
+
+};
+
+
+#endif //GITTESTS_PULLREQTEST_H
---
+++ pullreqTest.mm
@@ -0,0 +1,6 @@
+//
+// Created by Matsumoto Ryo on 2017/12/11.
+// Copyright (c) 2017 Univ-ryukyu. All rights reserved.
+//
+
+#include "pullreqTest.h"
